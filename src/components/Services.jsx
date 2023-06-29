781816bd06e812f70d0602fae240cd4d32f62334
Vaishnavi <2020bit013@sggs.ac.in>
2023-06-29 13:49:36 +0530

    first commit

---
+++ src/components/Services.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Carousel} from "react-responsive-carousel";
+import img1 from '../assets/img_3.png';
+import img2 from '../assets/img_4.png';
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+const Services = () => {
+  return (
+    <div className='services'>
+        <Carousel infiniteLoop autoPlay showStatus={false} showArrows={false} showThumbs={false} interval={1000}>
+            <div>
+                <img src={img1} alt="Item1"/>
+                <p className='legend'>Full Stack</p>
+            </div>
+            <div>
+                <img src={img2} alt="Item2"/>
+                <p className='legend'>Peer-to-Peer Support</p>
+            </div>
+        </Carousel>
+    </div>
+  )
+}
+
+export default Services
